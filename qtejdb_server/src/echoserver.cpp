c9e9dbd224d59452b06700e58176b86f417ab455
Markus Marx <markus.marx@netresearch.de>
2014-11-04 21:13:07 +0100

    add qtejdb_server path

---
+++ qtejdb_server/src/echoserver.cpp
@@ -0,0 +1,5 @@
+#include "echoserver.h"
+
+EchoServer::EchoServer()
+{
+}
---
+++ qtejdb_server/src/echoserver.h
@@ -0,0 +1,10 @@
+#ifndef ECHOSERVER_H
+#define ECHOSERVER_H
+
+class EchoServer
+{
+public:
+    EchoServer();
+};
+
+#endif // ECHOSERVER_H
